c53f587d53416c26ba7e31242211e637c45fa2d5
Art Navsegda <artnavsegda@gmail.com>
2015-11-29 03:31:09 +0300

    sdsd

--- src/conjunk3.c
+++ src/conjunk3.c
@@ -2,12 +2,12 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
 	char buf[100];
 	//char test;
         FILE *sora;
-        sora = fopen("./chota2.bin","r");
+        sora = fopen(argv[1],"rb");
         if (sora == NULL)
         {
                 printf("fucking error");
@@ -16,16 +16,21 @@ int main()
 
 	while (fread(buf,36,1,sora))
 	{
-		//if (buf[0] == (char)0xf4)
-		//	printf("%hhx start\n",buf[0]);
+		if (buf[0] == (char)0xf4)
+			printf("%hhx start\n",buf[0]);
 
-		printf("%hhx %hhx %hhx %hhx %hhx %hhx\n",buf[1],buf[2],buf[3],buf[4],buf[5],buf[6]);
+		printf("time is %hhx%hhx:%hhx%hhx:%hhx%hhx\n",buf[1]-0x30,buf[2]-0x30,buf[3]-0x30,buf[4]-0x30,buf[5]-0x30,buf[6]-0x30);
 
-		//if (buf[7] == (char)0xa5)
-		//	printf("%hhx marker\n",buf[7]);
-		//if (buf[35] == (char)0xf8)
-		//	printf("%hhx stop\n",buf[35]);
-		//puts("read");
+		if (buf[7] == (char)0xa5)
+			printf("%hhx marker\n",buf[7]);
+		printf("outgoing data %hhx %hhx %hhx %hhx %hhx %hhx %hhx %hhx %hhx %hhx %hhx %hhx %hhx %hhx %hhx %hhx %hhx %hhx %hhx %hhx %hhx %hhx\n", buf[8], buf[9], buf[10], buf[11], buf[12], buf[13], buf[14], buf[15], buf[16], buf[17], buf[18], buf[19], buf[20], buf[21], buf[22], buf[23], buf[24], buf[25], buf[26], buf[27], buf[28], buf[29]);
+		printf("first set is %hx\n",(unsigned short)buf[8]);
+		if (buf[30] == (char)0xb5)
+			printf("%hhx marker\n",buf[30]);
+		//printf("incoming data 
+		if (buf[35] == (char)0xf8)
+			printf("%hhx stop\n",buf[35]);
+		puts("read");
 	}
 		puts("read end");
 	return 0;
