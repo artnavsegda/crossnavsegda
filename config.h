fad90c0a5e783157b20b3bce13ecf0aeb5f05ae3
Art Navsegda <artnavsegda@gmail.com>
2016-08-16 13:40:12 +0300

    updooote

---
+++ config.h
@@ -0,0 +1,29 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Name of package */
+#define PACKAGE "artnavsegda"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "artnavsegda@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "ArtNavsegda"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "ArtNavsegda 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "artnavsegda"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "https://github.com/artnavsegda"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Version number of package */
+#define VERSION "1.0"
