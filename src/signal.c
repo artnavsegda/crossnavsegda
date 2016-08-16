f144c6b940b967b61976d151ad94387cff801b03
Art Navsegda <artnavsegda@gmail.com>
2016-08-16 12:34:54 +0300

    whaaaat

--- src/signal.c
+++ src/signal.c
@@ -2,15 +2,23 @@
 #include <signal.h>
 #include <unistd.h>
 
+int modenumber = 0;
+
 void alarm_handler(int signal)
 {
 	printf("alarm\n");
 	alarm(10);
 }
 
+void entermode(int modetoenter)
+{
+	modenumber = modetoenter;
+}
+
 int main(void)
 {
 	signal(SIGALRM, alarm_handler);
+	entermode(1);
 	alarm(10);
 	while (1)
 		sleep(1);
