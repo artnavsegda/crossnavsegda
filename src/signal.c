1ca319fa1b18f7a5bdd5441604051360ac0ff5ac
Art Navsegda <artnavsegda@gmail.com>
2016-08-16 12:19:44 +0300

    signaaaaal

---
+++ src/signal.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+void alarm_handler(int signal)
+{
+	printf("alarm\n");
+	alarm(10);
+}
+
+int main(void)
+{
+	signal(SIGALRM, alarm_handler);
+	alarm(10);
+	while (1)
+		sleep(1);
+	return 0;
+}
+
