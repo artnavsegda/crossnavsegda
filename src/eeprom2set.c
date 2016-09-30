d47252173e2b9cb84026068e94cc1f53d6937a6c
Art Navsegda <artnavsegda@gmail.com>
2016-09-30 11:15:43 +0300

    dis

--- src/eeprom2set.c
+++ src/eeprom2set.c
@@ -55,10 +55,11 @@ int main(void)
 		unsigned char ip[4];
 		unsigned char mac[6];
 		struct lengthtablestruct length_table;
-	  struct jumptablestruct jump_table;
+	 	struct jumptablestruct jump_table;
 	};
 	struct eeprom e;
-	fgets((char *)&e,sizeof(e),stdin);
+	//fgets((char *)&e,sizeof(e),stdin);
+	fread((char *)&e,sizeof(e),1,stdin);
 	printf("ip = %hhu.%hhu.%hhu.%hhu\n",e.ip[0],e.ip[1],e.ip[2],e.ip[3]);
 	printf("mac = %hhx:%hhx:%hhx:%hhx:%hhx:%hhx\n",e.mac[0],e.mac[1],e.mac[2],e.mac[3],e.mac[4],e.mac[5]);
 	printf("startlevel = %hu\nprecalibrationdelay = %hu\ncalibration = %hu\npostcalibrationdelay = %hu\ncelldelay = %hu\ncelllevel = %hu\nzerodelay = %hu\nzerotest = %hu\ntotalmercurydelay = %hu\ntotalmercury = %hu\nelementalmercurydelay = %hu\nelementalmercury = %hu\npurge = %hu\n",e.length_table.startlevel,e.length_table.precalibrationdelay,e.length_table.calibration,e.length_table.postcalibrationdelay,e.length_table.celldelay,e.length_table.celllevel,e.length_table.zerodelay,e.length_table.zerotest,e.length_table.totalmercurydelay,e.length_table.totalmercury,e.length_table.elementalmercurydelay,e.length_table.elementalmercury,e.length_table.purge);
--- src/set2eeprom.c
+++ src/set2eeprom.c
@@ -62,6 +62,6 @@ int main(void)
 	scanf("mac = %hhx:%hhx:%hhx:%hhx:%hhx:%hhx\n",&e.mac[0],&e.mac[1],&e.mac[2],&e.mac[3],&e.mac[4],&e.mac[5]);
 	scanf("startlevel = %hu\nprecalibrationdelay = %hu\ncalibration = %hu\npostcalibrationdelay = %hu\ncelldelay = %hu\ncelllevel = %hu\nzerodelay = %hu\nzerotest = %hu\ntotalmercurydelay = %hu\ntotalmercury = %hu\nelementalmercurydelay = %hu\nelementalmercury = %hu\npurge = %hu\n",&e.length_table.startlevel,&e.length_table.precalibrationdelay,&e.length_table.calibration,&e.length_table.postcalibrationdelay,&e.length_table.celldelay,&e.length_table.celllevel,&e.length_table.zerodelay,&e.length_table.zerotest,&e.length_table.totalmercurydelay,&e.length_table.totalmercury,&e.length_table.elementalmercurydelay,&e.length_table.elementalmercury,&e.length_table.purge);
 	//fputs((char *)&e,stdout);
-	fwrite((char *)&e,36,1,stdout);
+	fwrite((char *)&e,sizeof(e),1,stdin);
 	return 0;
 }
