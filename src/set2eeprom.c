9e8a80e94ab709c0e644be8c2a901966a9b31641
Art Navsegda <artnavsegda@gmail.com>
2016-10-01 07:11:45 +0300

    this

--- src/set2eeprom.c
+++ src/set2eeprom.c
@@ -48,15 +48,32 @@ struct jumptablestruct {
   enum modelist postcalibrationdelay;
 };
 
+struct jumptablestruct startstate = {
+	.startlevel = TOTALMERCURY,
+	.celldelay = TOTALMERCURY,
+	.celllevel = TOTALMERCURY,
+	.zerodelay = TOTALMERCURY,
+	.zerotest = TOTALMERCURY,
+	.purge = TOTALMERCURY,
+	.totalmercurydelay = TOTALMERCURY,
+	.totalmercury = TOTALMERCURY,
+	.elementalmercurydelay = TOTALMERCURY,
+	.elementalmercury = TOTALMERCURY,
+	.precalibrationdelay = TOTALMERCURY,
+	.calibration = TOTALMERCURY,
+	.postcalibrationdelay = TOTALMERCURY
+};
+
+struct eeprom
+{
+	unsigned char ip[4];
+	unsigned char mac[6];
+	struct lengthtablestruct length_table;
+	struct jumptablestruct jump_table;
+};
+
 int main(void)
 {
-	struct eeprom
-	{
-		unsigned char ip[4];
-		unsigned char mac[6];
-		struct lengthtablestruct length_table;
-		struct jumptablestruct jump_table;
-	};
 	struct eeprom e;
 	scanf("ip = %hhu.%hhu.%hhu.%hhu\n",&e.ip[0],&e.ip[1],&e.ip[2],&e.ip[3]);
 	scanf("mac = %hhx:%hhx:%hhx:%hhx:%hhx:%hhx\n",&e.mac[0],&e.mac[1],&e.mac[2],&e.mac[3],&e.mac[4],&e.mac[5]);
