4fdf1391a18f20b7d111a6be5bc29d3ad188b84e
Art Navsegda <artnavsegda@gmail.com>
2016-11-24 18:00:34 +0300

    overflow

--- src/overflow.c
+++ src/overflow.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+
+struct ra915data {
+	unsigned short pmt_current;
+	unsigned short flow_rate;
+	unsigned short pmt_voltage;
+	short concentration;
+	unsigned short bypass_pressure;
+	unsigned short t_analytical_cell;
+	unsigned short t_selftest_cell;
+	unsigned short pressure_analytical_cell;
+	unsigned short vacuum;
+	unsigned short dilution_pressure;
+	unsigned char status;
+};
+
+struct ra915struct {
+	unsigned char marker;
+	struct ra915data data;
+	unsigned char checksum;
+};
+
 unsigned char genchecksum(unsigned char *massive)
 {
 	unsigned char checksum = 0;
@@ -8,5 +30,9 @@ unsigned char genchecksum(unsigned char *massive)
 
 int main(void)
 {
+	struct ra915struct frame = {
+		.marker = 0xA5
+	};
+	frame.checksum = genchecksum((unsigned char *)&frame.data);
 	return 0;
 }
