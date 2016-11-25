75789f7b5227e95427401b30807dbe5cc94fa84a
Art Navsegda <artnavsegda@gmail.com>
2016-11-25 17:40:19 +0300

    serial

--- src/serial.c
+++ src/serial.c
@@ -2,9 +2,16 @@
 
 int main(int argc, char *argv[])
 {
+	int fd;
 	if (argc != 2)
 	{
 		printf("name serial port\n");
+		exit(1);
+	}
+	fd = open(argv[1],O_RDWR | O_NOCTTY);
+	if (fd == -1)
+	{
+		printf("error open serial port\n");
 		exit(1);
 	}
 	return 0;
