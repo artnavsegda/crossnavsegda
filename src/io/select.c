cc11a8c7101413b3d7f6894bb5443388addd02b0
Art Navsegda <artnavsegda@gmail.com>
2016-12-10 08:47:41 +0300

    select

--- src/io/select.c
+++ src/io/select.c
@@ -1,7 +1,12 @@
-#include <sys/time.h>
+#include <sys/select.h>
+#include <unistd.h>
 
 int main(void)
 {
+	fd_set rfds;
+	FD_ZERO(&rfds);
+	FD_SET(STDIN_FILENO,&rfds);
 	struct timeval tv = { .tv_sec = 5, .tv_usec = 0 };
+	select(1,&rfds,NULL,NULL,&tv);
 }
 
