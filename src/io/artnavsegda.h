e0282ae9cf74d41ab369c63c29ef033f4d09369b
Art Navsegda <artnavsegda@gmail.com>
2016-12-10 04:20:26 +0300

    lel

--- src/io/artnavsegda.h
+++
@@ -1,3 +0,0 @@
-#define artnavsegda "ARTNAVSEGDA"
-#define notnavsegda "NOTNAVSEGDA"
-
--- src/io/read.c
+++ src/io/read.c
@@ -1,45 +1,8 @@
-//art navsegda image format
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "artnavsegda.h"
 
 int main()
 {
-	char r,g,b;
-	int x = 0;
-	int y = 0;
-	int width;
-	int height;
-	int magiclength = strlen(artnavsegda);
-	char magic[sizeof(artnavsegda)];
-	fgets(magic,magiclength+1,stdin);
-	if (strcmp(artnavsegda,magic)!=0)
-	{
-		printf("incorrect format\n");
-		exit(1);
-	}
-
-	width = fgetc(stdin);
-	printf("width %d\n",width);
-	height = fgetc(stdin);
-	printf("height %d\n",height);
-
-	while (x != width)
-	{
-		y = 0;
-		while (y != height)
-		{
-			r = fgetc(stdin);
-			fputc(r,stdout);
-			g = fgetc(stdin);
-			fputc(g,stdout);
-			b = fgetc(stdin);
-			fputc(b,stdout);
-			y++;
-		}
-		fputs("SCANLINE",stdout);
-		x++;
-	}
 	return 0;
 }
