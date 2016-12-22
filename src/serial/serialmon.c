7019facd98923bad04e21e6c85a3a5b5a6199856
Art Navsegda <artnavsegda@gmail.com>
2016-12-22 10:42:32 +0300

    traplord

--- src/serial/serialmon.c
+++ src/serial/serialmon.c
@@ -14,5 +14,7 @@ int main(void)
 	while (scanf("%lu:RX%hhu:%hhX",&datamassive[position].millis,&datamassive[position].rxnum,&datamassive[position].recbyte) != EOF)
 		position++;
 	printf("%d record read\n",position);
+	for (int i=0; i<position; i++)
+		printf("%lu:RX%hhu:%hhX\n", datamassive[i].millis,datamassive[i].rxnum,datamassive[i].recbyte);
 	return 0;
 }
