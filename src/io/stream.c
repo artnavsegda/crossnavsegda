82f4799daa54ca6aa637b883d58cd652cfe303dc
Art Navsegda <artnavsegda@gmail.com>
2016-12-23 09:45:21 +0300

    stream

--- src/io/stream.c
+++ src/io/stream.c
@@ -2,6 +2,19 @@
 
 int main(int argc, char *argv[])
 {
+	if (argc != 2)
+	{
+		printf("enter file name\n");
+		return 1;
+	}
+
+	FILE * mystream = fopen(argv[1],"r");
+	if (mystream == NULL)
+	{
+		perror("open file");
+		return 1;
+	}
+
 	return 0;
 }
 
