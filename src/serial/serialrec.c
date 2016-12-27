93a090dda482ef1c76332a71d0392e66e1f2a1bf
Art Navsegda <artnavsegda@gmail.com>
2016-12-27 18:11:11 +0300

    commit

---
+++ src/serial/serialrec.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <strings.h>
+
+int main(int argc, char *argv[])
+{
+	unsigned char frame[22];
+	int fd;
+	//struct termios tio;
+	struct termios tio = {
+		.c_cflag = B115200 | CS8 | CLOCAL | CREAD,
+		.c_iflag = IGNPAR,
+		.c_oflag = 0,
+		.c_lflag = 0,
+	       	.c_cc[VTIME] = 100,
+		.c_cc[VMIN] = 22
+	};
+
+	if (argc != 2)
+	{
+		printf("name serial port\n");
+		exit(1);
+	}
+
+	fd = open(argv[1],O_RDWR | O_NOCTTY);
+
+	if (fd == -1)
+	{
+		perror("error open serial port");
+		exit(1);
+	}
+	else
+		printf("serial port open\n");
+
+	cfsetspeed(&tio,B115200);
+	tcsetattr(fd,TCSANOW,&tio);
+
+	while (1)
+	{
+		if (read(fd,frame,1) == 1)
+			write(STDOUT_FILENO,frame,1);
+	}
+
+	return 0;
+}
