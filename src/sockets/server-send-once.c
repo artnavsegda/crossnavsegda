d5d6bf3c4d3e4bfb2bad339ade8bf434ef5ad80f
Art Navsegda <artnavsegda@gmail.com>
2016-12-30 09:20:18 +0300

    kokokokok

---
+++ src/sockets/server-send-once.c
@@ -0,0 +1,86 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+
+int main()
+{
+	int buf[100];
+	int sock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+	if (sock == -1)
+	{
+		perror("socket error");
+		return 1;
+	}
+	else
+	{
+		printf("socket ok\n");
+	}
+
+	struct sockaddr_in server = {
+		.sin_family = AF_INET,
+		.sin_addr.s_addr = INADDR_ANY,
+		.sin_port = htons(502)
+	};
+
+	if (bind(sock,(struct sockaddr *)&server,sizeof(server)) == -1)
+	{
+		perror("bind error");
+		close(sock);
+		return 1;
+	}
+	else
+	{
+		printf("bind ok\n");
+	}
+
+	if (listen(sock,10) == -1)
+	{
+		perror("listen error");
+		close(sock);
+		return 1;
+	}
+	else
+	{
+		printf("listen ok\n");
+	}
+
+	while(1)
+	{
+		int msgsock = accept(sock,NULL,NULL);
+		if (msgsock == -1)
+		{
+			perror("accept error");
+			close(sock);
+			return 1;
+		}
+		else
+		{
+			printf("listen ok\n");
+		}
+
+		while (send(msgsock,"hello\n",6,0) != -1)
+			;
+		if (shutdown(msgsock, 2) == -1)
+		{
+			perror("shutdown error");
+			close(msgsock);
+			close(sock);
+			return 1;
+		}
+		else
+		{
+			printf("listen ok\n");
+		}
+		close(msgsock);
+	}
+	close(sock);
+
+	return 0;
+}
+
