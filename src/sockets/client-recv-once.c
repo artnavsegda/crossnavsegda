05a8217dfe3c1e2326927fce633c2b040c6b9aae
Art Navsegda <artnavsegda@gmail.com>
2016-12-31 01:17:17 +0300

    dsadasd2

---
+++ src/sockets/client-recv-once.c
@@ -0,0 +1,56 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+
+char buf[100];
+
+int main()
+{
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == -1)
+	{
+		perror("socket error");
+		return 1;
+	}
+	else
+	{
+		printf("socket ok\n");
+	}
+	struct sockaddr_in client = {
+		.sin_addr.s_addr = inet_addr("127.0.0.1"),
+		.sin_family = AF_INET,
+		.sin_port = htons(502)
+	};
+	if (connect(sock,(struct sockaddr *)&client, sizeof(client)) == -1)
+	{
+		perror("connect error");
+		close(sock);
+		return 1;
+	}
+	else
+	{
+		printf("connect ok\n");
+	}
+
+	int numread = recv(sock,buf,100,0);
+       	if (numread == -1)
+	{
+		perror("recv error");
+	}
+	else
+	{
+		printf("recv %d bytes ok\n");
+	}
+
+	shutdown(sock, 2);
+	close(sock);
+
+	return 0;
+}
+
