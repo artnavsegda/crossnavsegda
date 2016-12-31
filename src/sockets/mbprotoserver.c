f0d65381593c053c4b2ae754962d159a4be8b41d
Art Navsegda <artnavsegda@gmail.com>
2016-12-31 14:48:13 +0300

    schiza nakhooi

--- src/sockets/mbprotoserver.c
+++ src/sockets/mbprotoserver.c
@@ -32,6 +32,8 @@ struct mbframestruct {
 	struct pduframestruct pduframe;
 };
 
+struct mbframestruct askmbframe, reqmbframe;
+
 struct tcpframestruct tcpframe = {
 	.tsid = 1,
 	.protoid = 0,
@@ -103,7 +105,8 @@ int main()
 			printf("accept ok\n");
 		}
 
-		int numread = recv(msgsock,&askframe,6,0);
+		//int numread = recv(msgsock,&askframe,6,0);
+		int numread = recv(msgsock,&askmbframe,6,0);
 		if (numread == -1)
 		{
 			perror("recv error");
@@ -114,9 +117,12 @@ int main()
 		else
 		{
 			printf("recv %d bytes\n",numread);
-			printf("TS id: %d\n", bswap_16(askframe.tsid));
-			printf("Protocol id: %d\n", bswap_16(askframe.protoid));
-			printf("Length: %d\n", bswap_16(askframe.length));
+			//printf("TS id: %d\n", bswap_16(askframe.tsid));
+			printf("TS id: %d\n", bswap_16(askmbframe.tsid));
+			//printf("Protocol id: %d\n", bswap_16(askframe.protoid));
+			printf("Protocol id: %d\n", bswap_16(askmbframe.protoid));
+			//printf("Length: %d\n", bswap_16(askframe.length));
+			printf("Length: %d\n", bswap_16(askmbframe.length));
 			/*for (int i=0; i<numread;i++)
 			{
 				printf("0x%02X ",buf[i]);
@@ -124,7 +130,8 @@ int main()
 			printf("\n");*/
 		}
 
-		numread = recv(msgsock,&askpduframe,bswap_16(askframe.length),0);
+		//numread = recv(msgsock,&askpduframe,bswap_16(askframe.length),0);
+		numread = recv(msgsock,&askmbframe.pduframe,bswap_16(askmbframe.length),0);
 		if (numread == -1)
 		{
 			perror("recv error");
@@ -135,24 +142,36 @@ int main()
 		else
 		{
 			printf("recv %d bytes\n",numread);
-			printf("Unit id: %d\n", askpduframe.unitid);
-			printf("Function code: %d\n", askpduframe.fncode);
+			//printf("Unit id: %d\n", askpduframe.unitid);
+			printf("Unit id: %d\n", askmbframe.pduframe.unitid);
+			//printf("Function code: %d\n", askpduframe.fncode);
+			printf("Function code: %d\n", askmbframe.pduframe.fncode);
 			for (int i=0; i<(numread-2)/2;i++)
-				printf("%u ",bswap_16(askpduframe.data[i]));
+				printf("%u ",bswap_16(askmbframe.pduframe.data[i]));
+				//printf("%u ",bswap_16(askpduframe.data[i]));
 			printf("\n");
 		}
 
-		switch (askpduframe.fncode) {
+		switch (askmbframe.pduframe.fncode) {
 			case 1:
 			case 2:
 				// read coils/discrete inputs
 				// data[0] = address of first coil/discrete
 				// data[1] = number of colis to read
+				// reply
+				//askmbframe.pduframe.data[1] = generatecoils(askmbframe.pduframe.data[0],askmbframe.pduframe.data[1]);
+				//askmbframe.pduframe.data[0] = 1;
+				//askframe.length = 4;
 			case 3:
 			case 4:
 				// read holding/input registers
 				// data[0] = address of first register
 				// data[1] = number of registers to read
+				// reply
+				//askmbframe.pduframe.data[0] = askmbframe.pduframe.data[1];
+				//for (int i = 1;i>=bswap_16(askmbframe.pduframe.data[1]);i++)
+				//	askmbframe.pduframe.data[i] = registers[i+bswap_16(askmbframe.pduframe.data[1])];
+				//askframe.length = 3+(bswap_16(askmbframe.pduframe.data[1])*2);
 			case 5:
 				// write coil
 				// data[0] = address of coil
@@ -162,10 +181,11 @@ int main()
 				// data[0] = address of holding
 				// data[1] = value of holding to write
 			break;
-
 		}
 
-		int numwrite = send(msgsock,data,12,0);
+		int replylength = bswap_16(askframe.length) + 6;
+
+		int numwrite = send(msgsock,&askmbframe,replylength,0);
 		if (numwrite == -1)
 		{
 			perror("send error");
