e61e21eda8fa5bcb771d3877dbe06cdea24a9e87
Art Navsegda <artnavsegda@gmail.com>
2017-01-03 15:12:13 +0300

    amount

--- src/sockets/mbprotoserver.c
+++ src/sockets/mbprotoserver.c
@@ -208,7 +208,7 @@ int main()
 				//same as request
 				break;
 			case 6:
-				if (ntohs(askmbframe.pdu.data.writereg.regaddress) < 100)
+				if (ntohs(askmbframe.pdu.data.writereg.regaddress) < amount)
 					table[ntohs(askmbframe.pdu.data.writereg.regaddress)] = ntohs(askmbframe.pdu.data.writereg.regvalue);
 				break;
 			case 15:
@@ -216,7 +216,7 @@ int main()
 				break;
 			case 16:
 				for (int i = 0; i<ntohs(askmbframe.pdu.data.writemultireg.regnumber);i++)
-					if(ntohs(askmbframe.pdu.data.writemultireg.firstreg)+i < 100)
+					if(ntohs(askmbframe.pdu.data.writemultireg.firstreg)+i < amount)
 						table[ntohs(askmbframe.pdu.data.writemultireg.firstreg)+i] = ntohs(askmbframe.pdu.data.writemultireg.registers[i]);
 				askmbframe.length = htons(6);
 				break;
