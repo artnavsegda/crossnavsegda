bcd60b5ead243eb8306ab1feacd3a042fe59643e
Art Navsegda <artnavsegda@gmail.com>
2017-01-03 10:55:56 +0300

    shit

---
+++ src/sockets/webserver.c
@@ -0,0 +1,118 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+
+unsigned char data[12] = { 0x00, 0x01, 0x00, 0x00, 0x00, 0x05, 0xFF, 0x03, 0x02, 0x00, 0x00 };
+
+int main()
+{
+	unsigned char buf[100];
+	int sock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+	if (sock == -1)
+	{
+		perror("socket error");
+		return 1;
+	}
+	else
+	{
+		printf("socket ok\n");
+	}
+	int optval = 1;
+	setsockopt(sock,SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+
+	struct sockaddr_in server = {
+		.sin_family = AF_INET,
+		.sin_addr.s_addr = INADDR_ANY,
+		.sin_port = htons(80)
+	};
+
+	if (bind(sock,(struct sockaddr *)&server,sizeof(server)) == -1)
+	{
+		perror("bind error");
+		close(sock);
+		return 1;
+	}
+	else
+	{
+		printf("bind ok\n");
+	}
+
+	if (listen(sock,10) == -1)
+	{
+		perror("listen error");
+		close(sock);
+		return 1;
+	}
+	else
+	{
+		printf("listen ok\n");
+	}
+
+	//while(1)
+	//{
+		int msgsock = accept(sock,NULL,NULL);
+		if (msgsock == -1)
+		{
+			perror("accept error");
+			close(sock);
+			return 1;
+		}
+		else
+		{
+			printf("accept ok\n");
+		}
+
+		int numread = recv(msgsock,buf,100,0);
+		if (numread == -1)
+		{
+			perror("recv error");
+			close(msgsock);
+			close(sock);
+			return 1;
+		}
+		else
+		{
+			printf("recv %d bytes\n",numread);
+			for (int i=0; i<numread;i++)
+			{
+				printf("0x%02X ",buf[i]);
+			}
+			printf("\n");
+		}
+		int numwrite = send(msgsock,data,12,0);
+		if (numwrite == -1)
+		{
+			perror("send error");
+			close(msgsock);
+			close(sock);
+			return 1;
+		}
+		else
+		{
+			printf("send %d bytes ok\n",numwrite);
+		}
+
+		if (shutdown(msgsock, 2) == -1)
+		{
+			perror("shutdown error");
+			close(msgsock);
+			close(sock);
+			return 1;
+		}
+		else
+		{
+			printf("shutdown ok\n");
+		}
+		close(msgsock);
+	//}
+	close(sock);
+
+	return 0;
+}
+
