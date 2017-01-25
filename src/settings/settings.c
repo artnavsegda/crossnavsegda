fd7f95ce1e746495df2c3a4e58be36f6eb152e30
Art Navsegda <artnavsegda@gmail.com>
2017-01-25 17:05:23 +0300

    kek

--- src/settings/settings.c
+++ src/settings/settings.c
@@ -4,9 +4,19 @@
 
 char str[] = "one=1 two=2 three=3.14";
 
+
+char * getopt(char *config, char *token)
+{
+	char * pch = strtok(strstr(config,token),"=");
+	printf("token %s, ",pch);
+	pch = strtok(NULL," ");
+	printf("string value %s, ",pch);
+	return pch;
+}
+
 int main(void)
 {
-	char * pch = strtok(strstr(str,"two"),"=");
+/*	char * pch = strtok(strstr(str,"two"),"=");
 	printf("token %s, ",pch);
 	pch = strtok(NULL," ");
 	printf("string value %s, ",pch);
@@ -16,7 +26,11 @@ int main(void)
 	printf("token %s, ",pch);
 	pch = strtok(NULL," ");
 	printf("string value %s, ",pch);
-	printf("float value %f\n",atof(pch));
+	printf("float value %f\n",atof(pch));*/
+
+	printf("int value %d\n",atoi(getopt(str,"one")));
+	printf("int value %d\n",atoi(getopt(str,"two")));
+	printf("float value %f\n",atof(getopt(str,"three")));
 
 	return 0;
 }
