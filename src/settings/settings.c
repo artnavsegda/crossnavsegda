fee6f719298c34a8eff588cc78f34bcda11e2f27
Art Navsegda <artnavsegda@gmail.com>
2017-01-25 09:10:30 +0300

    settings

--- src/settings/settings.c
+++ src/settings/settings.c
@@ -1,13 +1,23 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+char str[] = "one=1 two=2 three=3.14";
 
 int main(void)
 {
-	char str[] = "one=1 two=2 three=3";
 	char * pch = strtok(strstr(str,"two"),"=");
 	printf("token %s, ",pch);
 	pch = strtok(NULL," ");
-	printf("value %s\n",pch);
+	printf("string value %s, ",pch);
+	printf("int value %d\n",atoi(pch)); 
+
+	pch = strtok(strstr(str,"three"),"=");
+	printf("token %s, ",pch);
+	pch = strtok(NULL," ");
+	printf("string value %s, ",pch);
+	printf("float value %f\n",atof(pch));
+
 	return 0;
 }
 
