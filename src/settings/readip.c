28179e960e653f63167f8121091fbb8716b5ec0d
Art Navsegda <artnavsegda@gmail.com>
2017-01-25 08:43:28 +0300

    kek

--- src/settings/readip.c
+++ src/settings/readip.c
@@ -2,8 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+char ipin[] = "192.168.1.150";
+unsigned char ip[4];
+char macin[] = "de:ad:be:ef:fe:ed";
+char mac[6];
+
 int main(void)
 {
+	ip[0] = atoi(strtok(ipin,"."));
+	ip[1] = atoi(strtok(NULL,"."));
+	ip[2] = atoi(strtok(NULL,"."));
+	ip[3] = atoi(strtok(NULL,"."));
+
+	printf("%hhu.%hhu.%hhu.%hhu\n",ip[0],ip[1],ip[2],ip[3]);
+
 	return 0;
 }
 
