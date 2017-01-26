162f768b9f3b1fd3c9ca7a28f355d18fd8a77e3f
Art Navsegda <artnavsegda@gmail.com>
2017-01-26 16:55:22 +0300

    settings

--- src/settings/settings.c
+++ src/settings/settings.c
@@ -2,15 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char str[] = "one=1 two=2 three=3.14 ip=192.168.1.150";
+//char str[100] = "one=1 two=2 three=3.14 ip=192.168.1.150";
+char str[100];
 
 char * getopt(char *config2, char *token)
 {
+	if (strstr(config2,token)==NULL)
+		return "0";
 	static char config[100];
 	strcpy(config,config2);
 	char * pch = strtok(strstr(config,token),"=");
 	printf("token %s, ",pch);
-	pch = strtok(NULL," ");
+	pch = strtok(NULL," \n");
 	printf("string value %s, ",pch);
 	return pch;
 }
@@ -28,15 +31,18 @@ char * getip(char *config2, char *token)
 int main(void)
 {
 	FILE * setfile = fopen("./settings.txt","r");
+	if (setfile != NULL)
+	{
+		fread(str,1,100,setfile);
+	}
 
+	printf("one int value %d\n",atoi(getopt(str,"one")));
+	printf("two int value %d\n",atoi(getopt(str,"two")));
+//	printf("one int value %d\n",atoi(getopt(str,"one")));
+//	printf("three float value %f\n",atof(getopt(str,"three")));
 
-	printf("int value %d\n",atoi(getopt(str,"one")));
-	printf("int value %d\n",atoi(getopt(str,"two")));
-	printf("int value %d\n",atoi(getopt(str,"one")));
-	printf("float value %f\n",atof(getopt(str,"three")));
-
-	char *ip = getip(str,"ip");
-	printf("ip %hhu.%hhu.%hhu.%hhu\n",ip[0],ip[1],ip[2],ip[3]);
+//	char *ip = getip(str,"ip");
+//	printf("ip %hhu.%hhu.%hhu.%hhu\n",ip[0],ip[1],ip[2],ip[3]);
 
 	return 0;
 }
