f9155a460f924d59788894a8ec592d01941e4284
Art Navsegda <artnavsegda@gmail.com>
2017-04-04 00:57:20 +0300

    ump

---
+++ src/sockets/udp-send-once.c
@@ -0,0 +1,74 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+
+int main()
+{
+	unsigned char buf[100];
+	int sock = socket(AF_INET,SOCK_STREAM,IPPROTO_UDP);
+	if (sock == -1)
+	{
+		perror("socket error");
+		return 1;
+	}
+	else
+	{
+		printf("socket ok\n");
+	}
+
+	struct sockaddr_in server = {
+		.sin_family = AF_INET,
+		.sin_addr.s_addr = INADDR_ANY,
+		.sin_port = htons(1234)
+	};
+
+	struct sockaddr_in other;
+	int slen = sizeof(other);
+
+	if (bind(sock,(struct sockaddr *)&server,sizeof(server)) == -1)
+	{
+		perror("bind error");
+		close(sock);
+		return 1;
+	}
+	else
+	{
+		printf("bind ok\n");
+	}
+
+	int numread = recvfrom(sock,buf,100,0,(struct sockaddr *)&other, &slen);
+	if (numread == -1)
+	{
+		perror("recv error");
+	}
+	else
+	{
+		printf("recv %d bytes\n",numread);
+		for (int i=0; i<numread;i++)
+		{
+			printf("0x%02X ",buf[i]);
+		}
+		printf("\n");
+	}
+
+	if (shutdown(sock, 2) == -1)
+	{
+		perror("shutdown error");
+		close(sock);
+		return 1;
+	}
+	else
+	{
+		printf("shutdown ok\n");
+	}
+	close(sock);
+
+	return 0;
+}
+
