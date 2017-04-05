9368e06be13a2137c78989b36854d97189b0a71b
Art Navsegda <artnavsegda@gmail.com>
2017-04-05 03:44:38 +0300

    ntp

--- src/time.c
+++ src/time.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 #include <time.h>
 
+#define NTP_TIME_OFFSET 2208988800
+
+struct ntptime {
+	unsigned int timeseconds;
+	unsigned int timefraction;
+};
+
+struct ntpframe {
+	unsigned char leapvermode;
+	unsigned char stratumlevel;
+	unsigned char poll;
+	unsigned char precision;
+	unsigned int rootdelay;
+	unsigned int driftrate;
+	unsigned int referenceid;
+	struct ntptime reference;
+	struct ntptime origin;
+	struct ntptime receive;
+	struct ntptime transmit;
+};
+
 int main(void)
 {
-	struct timespec timenow;
-	clock_gettime(CLOCK_REALTIME, &timenow);
-	printf("%d.%d\n",timenow.tv_sec,timenow.tv_nsec);
+	time_t timenow;
+	timenow = time(NULL);
+	printf("Unixtime: %ld\n",timenow);
+	printf("NTP time: %ld\n",timenow+NTP_TIME_OFFSET);
+	printf("NTP frame: %ld\n",sizeof(struct ntpframe));
 }
 
