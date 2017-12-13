63cdb4ad09ea243f215c3815be49d685c8d61662
Art Navsegda <artnavsegda@gmail.com>
2017-12-13 07:05:33 +0300

    modbusrtu

---
+++ src/modbusrtu/mbclient.c
@@ -0,0 +1,197 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <termios.h>
+#include "modbus.h"
+
+unsigned short CRC16 (const unsigned char *nData, unsigned short wLength);
+
+char buf[100];
+char ask[12] = { 0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x32, 0x03, 0x00, 0x00, 0x00, 0x01 };
+
+mbframestruct mbframe = {
+	.unitid = 50
+};
+
+mbframestruct askframe;
+
+void printbinary(unsigned char value)
+{
+	for (int i = 0; i < 8; i++)
+		printf("%u", (value >> (7-i)) & 0x01);
+}
+
+int main(int argc, char *argv[])
+{
+        struct termios tio = {
+                .c_cflag = B9600 | CS8 | CLOCAL | CREAD,
+                .c_iflag = IGNPAR,
+                .c_oflag = 0,
+                .c_lflag = 0,
+                .c_cc[VTIME] = 1,
+                .c_cc[VMIN] = 7
+        };
+
+	if (argc < 3)
+	{
+		printf("name serial port function code and its values\n");
+		printf("1 first amount: read coil\n");
+		printf("2 first amount: read discrete\n");
+		printf("3 first amount: read holding\n");
+		printf("4 first amount: read input\n");
+		printf("5 number value: write coil\n");
+		printf("6 number value: write holding\n");
+		printf("15 first amount number value: write miltiple coil\n");
+		printf("16 first amount number value: write miltiple holding\n");
+		return 1;
+	}
+
+	sscanf(argv[2],"%hhu",&mbframe.fncode); // <-----
+
+	switch (mbframe.fncode)
+	{
+		case 1:
+		case 2:
+			if (argc < 5)
+			{
+				printf("name first coil/discrete and amount to return\n");
+				return 1;
+			}
+			sscanf(argv[3],"%hu",&mbframe.data.askreadregs.firstreg);
+			mbframe.data.askreadregs.firstreg = htons(mbframe.data.askreadregs.firstreg);
+			sscanf(argv[4],"%hu",&mbframe.data.askreadregs.regnumber);
+			mbframe.data.askreadregs.regnumber = htons(mbframe.data.askreadregs.regnumber);
+			break;
+		case 3:
+		case 4:
+			if (argc < 5)
+			{
+				printf("name first input/holding and amount to return\n");
+				return 1;
+			}
+			sscanf(argv[3],"%hu",&mbframe.data.askreadregs.firstreg);
+			mbframe.data.askreadregs.firstreg = htons(mbframe.data.askreadregs.firstreg);
+			sscanf(argv[4],"%hu",&mbframe.data.askreadregs.regnumber);
+			mbframe.data.askreadregs.regnumber = htons(mbframe.data.askreadregs.regnumber);
+			break;
+		case 5:
+		case 6:
+			if (argc < 5)
+			{
+				printf("name register number and value to set\n");
+				return 1;
+			}
+			sscanf(argv[3],"%hu",&mbframe.data.writereg.regaddress);
+			mbframe.data.writereg.regaddress = htons(mbframe.data.writereg.regaddress);
+			sscanf(argv[4],"%hu",&mbframe.data.writereg.regvalue);
+			mbframe.data.writereg.regvalue = htons(mbframe.data.writereg.regvalue);
+			break;
+		case 15:
+		case 16:
+			if (argc < 6)
+			{
+				printf("name first register, registers amount to set, number of bytes to follow and bytes with values\n");
+				return 1;
+			}
+			sscanf(argv[3],"%hu",&mbframe.data.writemulticoil.firstreg);
+			mbframe.data.writemulticoil.firstreg = htons(mbframe.data.writemulticoil.firstreg);
+			sscanf(argv[4],"%hu",&mbframe.data.writemulticoil.regnumber);
+			mbframe.data.writemulticoil.regnumber = htons(mbframe.data.writemulticoil.regnumber);
+			sscanf(argv[5],"%hhu",&mbframe.data.writemulticoil.bytestofollow);
+			for (int i=0;i<mbframe.data.writemulticoil.bytestofollow;i++)
+				sscanf(argv[4+i],"%hhu",&mbframe.data.writemulticoil.coils[i]);
+			break;
+		default:
+			printf("unknown function number");
+			return 1;
+			break;
+	}
+
+        int fd = open(argv[1], O_RDWR | O_NOCTTY ); 
+        if (fd  == -1)
+        {
+                perror("cannot open serial port");
+                exit(1);
+        }
+
+        tcflush(fd, TCIFLUSH);
+        tcsetattr(fd,TCSANOW,&tio);
+
+	mbframe.checksum = CRC16((char *)&mbframe,6);
+
+	printf("checksum %02X\n",mbframe.checksum);
+
+	ssize_t numwrite = write(fd,&mbframe,6);
+	if (numwrite == -1)
+	{
+		perror("send error");
+	}
+	else
+	{
+		printf("send %zd bytes ok\n", numwrite);
+	}
+
+	write(fd,&mbframe.checksum,2);
+
+	int numread = read(fd,&askframe,255);
+	if (numread == -1)
+	{
+		perror("recv error");
+		close(fd);
+		return 1;
+	}
+	else
+	{
+		printf("recv %zd bytes\n",numread);
+		printf("Unit id: %d\n", askframe.unitid);
+		printf("Function code: %d\n", askframe.fncode);
+		for (int i=0; i<numread;i++)
+			printf("0x%02hhX ",((char *)&askframe)[i]);
+		printf("\n");
+		switch(askframe.fncode)
+		{
+		case 1:
+		case 2:
+			printf("number of bytes containing bit values: %d\n",askframe.data.reqread.bytestofollow);
+			for (int i=0;i<askframe.data.reqread.bytestofollow;i++)
+			{
+				printf("0x%02hhX(",askframe.data.reqread.bytes[i]);
+				printbinary(askframe.data.reqread.bytes[i]);
+				printf(") ");
+			}
+			printf("\n");
+		break;
+		case 3:
+		case 4:
+			printf("number of registers: %d\n",askframe.data.reqread.bytestofollow/2);
+			for (int i=0;i<askframe.data.reqread.bytestofollow/2;i++)
+			{
+				printf("0x%04hX ",((short *)&askframe.data.reqread.bytes)[i]);
+			}
+			printf("\n");
+		break;
+		case 5:
+		case 6:
+			printf("register number %u\n", ntohs(askframe.data.writereg.regaddress));
+			printf("register value %u\n", ntohs(askframe.data.writereg.regvalue));
+		break;
+		case 15:
+		case 16:
+			printf("first register %u\n", ntohs(askframe.data.writemulticoil.firstreg));
+			printf("registers amount %u\n", ntohs(askframe.data.writemulticoil.regnumber));
+		break;
+		default:
+			printf("unknown function number");
+		break;
+		}
+	}
+
+	close(fd);
+
+	return 0;
+}
---
+++ src/modbusrtu/mbserver.c
@@ -0,0 +1,215 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include "modbus.h"
+
+mbframestruct askmbframe, reqmbframe;
+
+unsigned short table[100] = {0xABCD, 0xDEAD};
+unsigned char bctable[100] = {0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 ,0 , 0, 1 };
+unsigned short amount = 100;
+
+int main()
+{
+	int sock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+	if (sock == -1)
+	{
+		perror("socket error");
+		return 1;
+	}
+	else
+	{
+		printf("socket ok\n");
+	}
+	int optval = 1;
+	setsockopt(sock,SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+
+	struct sockaddr_in server = {
+		.sin_family = AF_INET,
+		.sin_addr.s_addr = INADDR_ANY,
+		.sin_port = htons(1100)
+	};
+
+	if (bind(sock,(struct sockaddr *)&server,sizeof(server)) == -1)
+	{
+		perror("bind error");
+		close(sock);
+		return 1;
+	}
+	else
+	{
+		printf("bind ok\n");
+	}
+
+	if (listen(sock,10) == -1)
+	{
+		perror("listen error");
+		close(sock);
+		return 1;
+	}
+	else
+	{
+		printf("listen ok\n");
+	}
+
+	while(1)
+	{
+		int msgsock = accept(sock,NULL,NULL);
+		if (msgsock == -1)
+		{
+			perror("accept error");
+			close(sock);
+			return 1;
+		}
+		else
+		{
+			printf("accept ok\n");
+		}
+
+		int numread = recv(msgsock,&askmbframe,6,0);
+		if (numread == -1)
+		{
+			perror("recv error");
+			close(msgsock);
+			close(sock);
+			return 1;
+		}
+		else
+		{
+			printf("recv %d bytes\n",numread);
+			printf("TS id: %d\n", ntohs(askmbframe.tsid));
+			printf("Protocol id: %d\n", ntohs(askmbframe.protoid));
+			printf("Length: %d\n", ntohs(askmbframe.length));
+		}
+
+		numread = recv(msgsock,&askmbframe.pdu,ntohs(askmbframe.length),0);
+		if (numread == -1)
+		{
+			perror("recv error");
+			close(msgsock);
+			close(sock);
+			return 1;
+		}
+		else
+		{
+			printf("recv %d bytes\n",numread);
+			printf("Unit id: %d\n", askmbframe.pdu.unitid);
+			printf("Function code: %d\n", askmbframe.pdu.fncode);
+			for (int i=0; i<(numread-2)/2;i++)
+				printf("%u ",ntohs(askmbframe.pdu.data.words[i]));
+			printf("\n");
+		}
+
+		int firstrequest = 0;
+		int requestnumber = 0;
+		switch (askmbframe.pdu.fncode) {
+			case 1:
+			case 2:
+				firstrequest = ntohs(askmbframe.pdu.data.askreadregs.firstreg);
+				printf("Requesting bits starting from: %d\n", firstrequest);
+				requestnumber = ntohs(askmbframe.pdu.data.askreadregs.regnumber);
+				printf("Number of bits requested: %d\n", requestnumber);
+				askmbframe.pdu.data.reqread.bytestofollow = requestnumber / 8;
+				if ((requestnumber % 8)>0)
+					askmbframe.pdu.data.reqread.bytestofollow++;
+				askmbframe.length = htons(askmbframe.pdu.data.reqread.bytestofollow + 3);
+				// fill all requested coil bytes with zeroes
+				for (int i = 0; i < askmbframe.pdu.data.reqread.bytestofollow;i++)
+					askmbframe.pdu.data.reqread.bytes[i] = 0;
+				// fill requested coil in bytes with bits
+				for (int i = 0; i < requestnumber;i++)
+					if (firstrequest+i < amount)
+						if (bctable[firstrequest+i] != 0)
+							askmbframe.pdu.data.reqread.bytes[i/8] = askmbframe.pdu.data.reqread.bytes[i/8] | (0x01 << i%8);
+			break;
+			case 3:
+			case 4:
+				firstrequest = ntohs(askmbframe.pdu.data.askreadregs.firstreg);
+				printf("Requesing register starting from: %d\n", firstrequest);
+				requestnumber = ntohs(askmbframe.pdu.data.askreadregs.regnumber);
+				printf("Number of registers requested: %d\n", requestnumber);
+				askmbframe.pdu.data.reqread.bytestofollow = requestnumber * 2;
+				askmbframe.length = htons(askmbframe.pdu.data.reqread.bytestofollow + 3);
+				// fill every requested register with table values
+				for (int i = 0; i < requestnumber;i++)
+					if (firstrequest+i < amount)
+						((unsigned short *)&askmbframe.pdu.data.reqread.bytes)[i] = htons(table[firstrequest+i]);
+					else
+						((unsigned short *)&askmbframe.pdu.data.reqread.bytes)[i] = htons(0x0000);
+				/*for (int i = 0; i < requestnumber;i++)
+					((unsigned short *)&askmbframe.pdu.data.reqread.bytes)[i] = htons(table[i]);*/
+			break;
+			case 5:
+				if (ntohs(askmbframe.pdu.data.writereg.regaddress) < amount)
+				{
+					if (askmbframe.pdu.data.writereg.regvalue == 0)
+						bctable[ntohs(askmbframe.pdu.data.writereg.regaddress)] = 0;
+					else
+						bctable[ntohs(askmbframe.pdu.data.writereg.regaddress)] = 1;
+				}
+				break;
+			case 6:
+				if (ntohs(askmbframe.pdu.data.writereg.regaddress) < amount)
+					table[ntohs(askmbframe.pdu.data.writereg.regaddress)] = ntohs(askmbframe.pdu.data.writereg.regvalue);
+				break;
+			case 15:
+				for (int i = 0; i<ntohs(askmbframe.pdu.data.writemultireg.regnumber);i++)
+					if(ntohs(askmbframe.pdu.data.writemultireg.firstreg)+i < amount)
+					{
+						if (askmbframe.pdu.data.writereg.regvalue == 0)
+							bctable[ntohs(askmbframe.pdu.data.writereg.regaddress)] = 0;
+						else
+							bctable[ntohs(askmbframe.pdu.data.writereg.regaddress)] = 1;
+					}
+				break;
+				askmbframe.length = htons(6);
+				break;
+			case 16:
+				for (int i = 0; i<ntohs(askmbframe.pdu.data.writemultireg.regnumber);i++)
+					if(ntohs(askmbframe.pdu.data.writemultireg.firstreg)+i < amount)
+						table[ntohs(askmbframe.pdu.data.writemultireg.firstreg)+i] = ntohs(askmbframe.pdu.data.writemultireg.registers[i]);
+				askmbframe.length = htons(6);
+				break;
+			break;
+		}
+
+		int replylength = ntohs(askmbframe.length) + 6;
+		printf("replylength %d\n",replylength);
+
+		int numwrite = send(msgsock,&askmbframe,replylength,0);
+		if (numwrite == -1)
+		{
+			perror("send error");
+			close(msgsock);
+			close(sock);
+			return 1;
+		}
+		else
+		{
+			printf("send %d bytes ok\n",numwrite);
+		}
+
+		//shutdown(msgsock, 2);
+		/*if (shutdown(msgsock, 2) == -1)
+		{
+			perror("shutdown error");
+			close(msgsock);
+			close(sock);
+			return 1;
+		}
+		else
+		{
+			printf("shutdown ok\n");
+		}*/
+		//close(msgsock);
+	}
+	close(sock);
+
+	return 0;
+}
