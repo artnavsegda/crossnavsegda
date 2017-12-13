7ad8d3f74a821588137b40d30b8f8a9c8d15ec80
Art Navsegda <artnavsegda@gmail.com>
2017-12-13 03:40:57 +0300

    blocked

--- src/serial/blocked.c
+++ src/serial/blocked.c
@@ -18,9 +18,19 @@ int main(int argc, char *argv[])
         int fd,c, res;
         struct termios oldtio,newtio;
         char buf[255];
+
+	if (argc != 2)
+	{
+		printf("name serial port\n");
+		exit(1);
+	}
         
-        fd = open(MODEMDEVICE, O_RDWR | O_NOCTTY ); 
-        if (fd <0) {perror(MODEMDEVICE); exit(-1); }
+        fd = open(argv[1], O_RDWR | O_NOCTTY ); 
+        if (fd  == -1)
+	{
+		perror("cannot open serial port");
+		exit(1);
+	}
         
         tcgetattr(fd,&oldtio); /* save current port settings */
         
@@ -39,10 +49,13 @@ int main(int argc, char *argv[])
         tcsetattr(fd,TCSANOW,&newtio);
         
         
-        while (1) {       /* loop for input */
-          res = read(fd,buf,255);   /* returns after 5 chars have been input */
-          buf[res]=0;               /* so we can printf... */
-          printf(":%s:%d\n", buf, res);
+        while (1)
+	{       /* loop for input */
+		res = read(fd,buf,255);   /* returns after 5 chars have been input */
+		buf[res]=0;               /* so we can printf... */
+		printf("\n:%d:", res);
+		for (int i = 0;i < res; i++)
+			printf("0x%02X ",buf[i]);
         }
         tcsetattr(fd,TCSANOW,&oldtio);
 }
