778ca1979534f589e995af2639e24f790daf41f8
Art Navsegda <artnavsegda@gmail.com>
2019-02-19 02:15:58 +0300

    fix

--- src/io/select.c
+++ src/io/select.c
@@ -7,7 +7,7 @@ int main(void)
 	fd_set rfds;
 	FD_ZERO(&rfds);
 	FD_SET(STDIN_FILENO,&rfds);
-	struct timeval tv = { .tv_sec = 5, .tv_usec = 0 };
+	struct timeval tv = { .tv_sec = 3, .tv_usec = 0 };
 	select(1,&rfds,NULL,NULL,&tv);
 
 	if (FD_ISSET(STDIN_FILENO, &rfds))
