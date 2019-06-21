1a8c266f9b25e7dabd7b6603b2393a713c8d6481
Art Navsegda <artnavsegda@gmail.com>
2019-06-21 16:07:09 +0300

    Create words.c

---
+++ src/words.c
@@ -0,0 +1,34 @@
+char array[5][5] = {
+	"aqwsz",
+	"bvghn",
+	"cxdfv",
+	"dserfcx",
+	"ewrsd",
+	"fdrtgvc",
+	"gftyhbv",
+	"hgyujnb",
+	"iuojk"
+	"jhuikmn"
+	"kjiolm",
+	"lkop",
+	"mnjk",
+	"nbhjm",
+	"oipkl",
+	"pol",
+	"qwa",
+	"retdf",
+	"sawedxz",
+	"tryfg",
+	"uyihj",
+	"vcfgb",
+	"wqeas",
+	"xzsdc",
+	"ytugh",
+	"zasx",
+};
+
+int main()
+{
+	return 0;
+}
+
