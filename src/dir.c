86afbf77b412eb5cf5ba59b9c251393b986694ba
Art Navsegda <artnavsegda@gmail.com>
2019-09-03 11:46:57 +0300

    dir

--- src/dir.c
+++ src/dir.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/param.h>
+#include <dirent.h>
 
 int main(void)
 {
 	char path[MAXPATHLEN];
 	getcwd(path,MAXPATHLEN);
 	printf("wd: %s\n",path);
+  DIR *d = opendir(path);
+  struct dirent *dir;
+  if (d)
+  {
+    while ((dir = readdir(d)) != NULL)
+    {
+      puts(dir->d_name);
+    }
+    closedir(d);
+  }
 	return 0;
 }
