6048dd72bde0e03d82ec3067c7a10ffc140d3c9d
Art Navsegda <artnavsegda@gmail.com>
2019-09-11 11:45:38 +0300

    all

--- src/forkexec.c
+++ src/forkexec.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 int main()
 {
   pid_t pid = fork();
+  int status;
 
   if (pid == 0)
   {
     puts("child process");
+    char * argv_list[] = {"ls","-lart","/home",NULL};
+    execv("/bin/ls",argv_list);
+    puts("ls before");
+    exit(0);
   }
   else
   {
     printf("parent process child pid is %d\n",pid);
+
+    waitpid(pid, &status,0);
+
+    exit(0);
+
   }
 
   return 0;
