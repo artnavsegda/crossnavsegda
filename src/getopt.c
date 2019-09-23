cd464025e2c98b9fa180fbf876c6d73397ecff71
Art Navsegda <artnavsegda@gmail.com>
2019-09-23 11:32:10 +0300

    getopt

--- src/getopt.c
+++ src/getopt.c
@@ -7,14 +7,19 @@ int main(int argc, char *argv[])
   int opt;
   char * config = NULL;
   char * type = NULL;
-  while ((opt = getopt(argc, argv, "nt")) != -1)
+  while ((opt = getopt(argc, argv, "c:t:")) != -1)
   {
     switch (opt)
     {
       case 'c': // config
+        config = optarg;
         break;
       case 't': // type
+        type = optarg;
         break;
     }
   }
+
+  printf("config %s, type %s\n", config, type);
+
 }
