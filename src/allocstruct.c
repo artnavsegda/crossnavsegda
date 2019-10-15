3199010bee73b39607220d536a68feb59780f4d5
Art Navsegda <artnavsegda@gmail.com>
2019-10-15 17:30:52 +0300

    pushe

--- src/allocstruct.c
+++ src/allocstruct.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+struct complement
+{
+  char * command;
+  enum domains domain;
+  char * description;
+};
+
+struct complestruct
+{
+  char * locode;
+  int complecount;
+  struct complement complelist;
+};
+
 int main()
 {
   printf("fuck");
