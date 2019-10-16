b0bfc97a2f891e8449dc25649707ac09200e5467
Art Navsegda <artnavsegda@gmail.com>
2019-10-16 13:58:12 +0300

    trip a

--- src/allocstruct.c
+++ src/allocstruct.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 enum domains { PROTO, FACE, OPTION, SETTING, COMMAND };
 
@@ -16,10 +17,33 @@ struct complestruct
   struct complement complelist[];
 };
 
+typedef char *callback_func_t (void);
 
+char ** array_allocate(callback_func_t *cb_func)
+{
+  char *string;
+  char **string_list = NULL;
+  //string_list = (char **)malloc(sizeof (char *));
+  int counter = 0;
+
+  while (string = (*cb_func)())
+  {
+    counter++;
+    string_list = (char **)realloc(string_list, sizeof(char *) * counter);
+    string_list[counter-1] = string;
+  }
+  printf("%d\n",counter);
+
+  for (int i = 0; i < counter; i++)
+  {
+    puts(string_list[i]);
+  }
+
+  return string_list;
+}
 
 int main()
 {
-  printf("fuck");
+  array_allocate(callback);
 	return 0;
 }
