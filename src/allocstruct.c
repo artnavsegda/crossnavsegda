9bcfecb4a9d2a68c6a0b03f3ec16ac6602d697b1
Art Navsegda <artnavsegda@gmail.com>
2019-10-16 15:12:59 +0300

    lol

--- src/allocstruct.c
+++ src/allocstruct.c
@@ -12,12 +12,12 @@ struct complement
 
 typedef struct complement cmpstr_t;
 
-struct complestruct
-{
-  char * locode;
-  int complecount;
-  cmpstr_t complelist[];
-};
+//struct complestruct
+//{
+//  char * locode;
+//  int complecount;
+//  cmpstr_t *complelist[];
+//};
 
 typedef cmpstr_t *callback_func_t (void);
 
@@ -33,28 +33,28 @@ cmpstr_t *callback(void)
     return NULL;
 }
 
-/*char ** array_allocate(callback_func_t *cb_func)
+char ** array_allocate(callback_func_t *cb_func)
 {
-  char *string;
-  char **string_list = NULL;
+  cmpstr_t *element;
+  cmpstr_t **list = NULL;
   //string_list = (char **)malloc(sizeof (char *));
   int counter = 0;
 
-  while (string = (*cb_func)())
-  {
-    counter++;
-    string_list = (char **)realloc(string_list, sizeof(char *) * counter);
-    string_list[counter-1] = string;
-  }
-  printf("%d\n",counter);
-
-  for (int i = 0; i < counter; i++)
-  {
-    puts(string_list[i]);
-  }
-
-  return string_list;
-}*/
+//  while (string = (*cb_func)())
+//  {
+//    counter++;
+//    string_list = (char **)realloc(string_list, sizeof(char *) * counter);
+//    string_list[counter-1] = string;
+//  }
+//  printf("%d\n",counter);
+//
+//  for (int i = 0; i < counter; i++)
+//  {
+//    puts(string_list[i]);
+//  }
+//
+//  return string_list;
+}
 
 int main()
 {
