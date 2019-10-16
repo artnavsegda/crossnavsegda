7de4412bb4fde1b66758d984802a9fa993c22bb7
Art Navsegda <artnavsegda@gmail.com>
2019-10-16 13:22:28 +0300

    push

--- src/allocstring.c
+++ src/allocstring.c
@@ -4,12 +4,21 @@ typedef char *callback_func_t (void);
 
 char * callback(void)
 {
-  puts("hoba");
+  static int i = 0;
+  i++;
+  if (i < 10)
+    return "hoba";
+  else
+    return NULL;
 }
 
 char ** array_allocate(callback_func_t *cb_func)
 {
-	(*cb_func)();
+  char *string;
+  while (string = (*cb_func)())
+  {
+    puts(string);
+  }
 }
 
 int main()
