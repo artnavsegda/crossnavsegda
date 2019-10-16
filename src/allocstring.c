b1ac8a3b3bc2b004eb555465b49427b9dd5d5bf4
Art Navsegda <artnavsegda@gmail.com>
2019-10-16 13:06:22 +0300

    callback

--- src/allocstring.c
+++ src/allocstring.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
-//char ** array_allocate(
+typedef void callback_func_t (void);
+
+void callback(void)
+{
+  puts("hoba");
+}
+
+char ** array_allocate(callback_func_t *cb_func)
+{
+	(*cb_func)();
+}
 
 int main()
 {
+	array_allocate(callback);
 	return 0;
 }
