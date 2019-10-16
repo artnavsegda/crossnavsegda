5c856e756c4c0d96ecb10a147ab8318858ce6b31
Art Navsegda <artnavsegda@gmail.com>
2019-10-16 13:00:33 +0300

    callback

--- src/callback.c
+++ src/callback.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
+typedef void callback_func_t (void);
+
 void callback(void)
 {
+  puts("hoba");
 }
 
-void recv_callback()
+void recv_callback(callback_func_t *cb_func)
 {
+  (*cb_func)();
 }
 
 int main()
 {
+  recv_callback(callback);
 	return 0;
 }
